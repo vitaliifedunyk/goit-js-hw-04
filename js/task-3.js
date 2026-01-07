e48cd4bbde9c277aa038f6399fdfbf3d1db981cc
vitaliifedunyk <vitalik.fedunyk@gmail.com>
2026-01-07 21:54:38 +0100

    task-3

--- js/task-3.js
+++ js/task-3.js
@@ -1,6 +1,15 @@
 const profile = {
   username: 'Jacob',
   playTime: 300,
+  changeUsername(newName) {
+    this.username = newName;
+  },
+  updatePlayTime(hours) {
+    this.playTime += hours;
+  },
+  getInfo() {
+    return `${this.username} has ${this.playTime} active hours!`;
+  },
 };
 
 console.log(profile.getInfo()); // "Jacob has 300 active hours!"
